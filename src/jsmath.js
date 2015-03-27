61e935f5d00dbca6d5b179c7c59351f637d60a41
ealgase <mostdigitsofpi@gmail.com>
2015-03-27 16:10:25 -0400

    Update jsmath.js

--- src/jsmath.js
+++ src/jsmath.js
@@ -6,7 +6,7 @@
     ext.pi = function(){return Math.PI}
     ext.e = function(){return Math.E}
     ext.math = function(num1,operation,num2){
-        if (operation=='+'){return num1+num2}
+        if (operation=='+'){return Number(num1)+Number(num2)}
         if (operation=='-'){return num1-num2}
         if (operation=='*'){return num1*num2}
         if (operation=='/'){return num1/num2}
@@ -22,5 +22,5 @@
         math: ['+','-','*','/','^'],
         }
     };
-    ScratchExtensions.register('Boost - JavaScript Math', descriptor, ext);
+    ScratchExtensions.register('Boost - JavaScript Math3', descriptor, ext);
 })({})
