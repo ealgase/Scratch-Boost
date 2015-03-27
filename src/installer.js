2f392d052401e221456574161a888c61ce89a37c
ealgase <mostdigitsofpi@gmail.com>
2015-03-27 15:56:01 -0400

    Update installer.js

--- src/installer.js
+++ src/installer.js
@@ -1,18 +1,18 @@
-ScratchExtensions.loadExternalJS('https://rawgit.com/Ealgase/Scratch-Boost/master/src/strings.js');
-ScratchExtensions.loadExternalJS('https://rawgit.com/Ealgase/Scratch-Boost/master/src/scratch.js');
-ScratchExtensions.loadExternalJS('https://rawgit.com/Ealgase/Scratch-Boost/master/src/misc.js');
-ScratchExtensions.loadExternalJS('https://rawgit.com/Ealgase/Scratch-Boost/master/src/local.js');
-ScratchExtensions.loadExternalJS('https://rawgit.com/Ealgase/Scratch-Boost/master/src/jscript.js');
-ScratchExtensions.loadExternalJS('https://rawgit.com/Ealgase/Scratch-Boost/master/src/browser.js');
-ScratchExtensions.loadExternalJS('https://rawgit.com/Ealgase/Scratch-Boost/master/src/color.js');
-ScratchExtensions.loadExternalJS('https://rawgit.com/Ealgase/Scratch-Boost/master/src/jsmath.js');
-ScratchExtensions.loadExternalJS('https://rawgit.com/Ealgase/Scratch-Boost/master/src/follow.js')
+ScratchExtensions.loadExternalJS('https://rawgit.com/Ealgase/Scratch-Boost/sandbox/src/strings.js');
+ScratchExtensions.loadExternalJS('https://rawgit.com/Ealgase/Scratch-Boost/sandbox/src/scratch.js');
+ScratchExtensions.loadExternalJS('https://rawgit.com/Ealgase/Scratch-Boost/sandbox/src/misc.js');
+ScratchExtensions.loadExternalJS('https://rawgit.com/Ealgase/Scratch-Boost/sandbox/src/local.js');
+ScratchExtensions.loadExternalJS('https://rawgit.com/Ealgase/Scratch-Boost/sandbox/src/jscript.js');
+ScratchExtensions.loadExternalJS('https://rawgit.com/Ealgase/Scratch-Boost/sandbox/src/browser.js');
+ScratchExtensions.loadExternalJS('https://rawgit.com/Ealgase/Scratch-Boost/sandbox/src/color.js');
+ScratchExtensions.loadExternalJS('https://rawgit.com/Ealgase/Scratch-Boost/sandbox/src/jsmath.js');
+ScratchExtensions.loadExternalJS('https://rawgit.com/Ealgase/Scratch-Boost/sandbox/src/follow.js')
 $(".box-head > .buttons").append('<div class="button" onclick="info()"><span class="white">Scratch Boost</span></div>');
 var inf = "";
 function info() {
     if (inf === "") {
         inf = $("#info").html();
-        $("#info").html('<div class="dynamic"><div class="text-block"><b>Scratch Boost Info</b><div class="read-only instructions" style="height: 161.5px;"><p>Scratch Boost by: Iwotastic<br />View on <a href="https://github.com/Ealgase/Scratch-Boost">GitHub</a><br /><br />Forked by Ealgase');    
+        $("#info").html('<div class="dynamic"><div class="text-block"><b>Scratch Boost Info</b><div class="read-only instructions" style="height: 161.5px;"><p>Scratch Boost by: Iwotastic<br />View on <a href="https://github.com/Ealgase/Scratch-Boost">GitHub</a><br /><br />Forked by Ealgase<br />You are using the sandbox version of Scratch Boost.  Some of the blocks may not work correctly.  Head over to <a href=/"ealgase.github.io/">the Scratch Boost installation page</a> to get the finished product.');    
       }else{
         $("#info").html(inf);
         inf = "";
