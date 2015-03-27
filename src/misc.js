2c13e144c42e0af607c6d7fc7eff70222164bfbe
ealgase <mostdigitsofpi@gmail.com>
2015-03-27 16:32:36 -0400

    Update misc.js

--- src/misc.js
+++ src/misc.js
@@ -70,13 +70,13 @@
             ['r', 'reporter comment %s', 'comr', 'Blah Blah Blah'],
             ['c', 'block comment', 'bcom'],
             ['h', 'whenever %b becomes true', 'when', null],
-            ['b', 'm.truefalse', 'tf', true],
+            ['b', '%m.truefalse', 'tf', true],
             //['r', 'join %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s', 'join', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
         ],
         menus: {
             inequals: ['≥', '≤', '≠'],
-            truefalse: [true,false]
+            truefalse: ['true','false'],
         }
     };
-    ScratchExtensions.register('Boost - Miscellaneous', descriptor, ext);
-})({}); 
+    ScratchExtensions.register('Boost - Miscellaneous3', descriptor, ext);
+})({});
