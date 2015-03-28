eff007e9a85b3eadf7bf027aa466b1abdf715157
ealgase <mostdigitsofpi@gmail.com>
2015-03-28 10:13:12 -0400

    Update misc.js

--- src/misc.js
+++ src/misc.js
@@ -3,7 +3,7 @@
     var readyS = true;
 	ext._shutdown = function() {};
     ext._getStatus = function() {
-        return {status: 1, msg: 'Some blocks may not work.'};
+        return {status: 2, msg: 'Ready'};
     };
     ext.countdown = function(time) {
         window.setTimeout(function(){
@@ -37,8 +37,6 @@
     ext.bi = function() {
         return true
     }
-    ext.com = function() {}
-    ext.bcom = function() {}
     ext.when = function(tf) {
         if(readyS){
             if(tf){
@@ -56,9 +54,7 @@
             }
         }
     }
-    ext.comr=function(){return ''};
     ext.tf=function(tof){return tof};
-    //ext.join=function(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z){return a.toString()+b+c+d+e+f+g+h+i+j+k+l+m+nn+o+p+q+r+s+t+u+v+w+x+y+z}
     var descriptor = {
         blocks: [
             ['h', 'when countdown goes off', 'countdownOff'],
@@ -66,17 +62,13 @@
             ['r', 'if %b then %s else %s', 'ifElseVal', null, '1', '0'],
             ['b', '%n %m.inequals %n', 'inequal', 2, '≠', 2],
             ['b', 'Scratch Boost installed?', 'bi'],
-            [' ', 'comment %s', 'com', 'Blah Blah Blah'],
-            ['r', 'reporter comment %s', 'comr', 'Blah Blah Blah'],
-            ['c', 'block comment', 'bcom'],
             ['h', 'whenever %b becomes true', 'when', null],
             ['b', '%m.truefalse', 'tf', true],
-            //['r', 'join %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s', 'join', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
         ],
         menus: {
             inequals: ['≥', '≤', '≠'],
             truefalse: ['true','false'],
         }
     };
-    ScratchExtensions.register('Boost - Miscellaneous3', descriptor, ext);
+    ScratchExtensions.register('Boost - Miscellaneous', descriptor, ext);
 })({});
